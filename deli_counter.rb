a706ef6aa666817887d57096c4ed1a8abf35496e
William Thompson <wmthmps1@gmail.com>
2018-02-28 21:19:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(array)
+    if array==[]
+      puts "The line is currently empty."
+    else
+      line_info=[]
+      count=0
+      array.collect do |customer|
+      line_info << "#{count+1}. #{customer}"
+      line_info.join(" ")
+      end
+     puts "The line is currently: #{line_info}"
+    end
+end
